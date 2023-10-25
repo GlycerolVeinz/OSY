2e5093c0e47fe4b8b0e95f977b4a156ad515f067
GlycerolVeinz <safrankou.matvei@gmail.com>
2023-10-25 11:31:06 +0200

    started HW03

---
+++ Homeworks/HW03/forkpipe.c
@@ -0,0 +1,12 @@
+//
+// Created by glycerolveinz on 25.10.23.
+//
+
+#include "forkpipe.h"
+
+int main(){
+    int ret = 0;
+    
+
+    return ret;
+}
---
+++ Homeworks/HW03/forkpipe.h
@@ -0,0 +1,8 @@
+//
+// Created by glycerolveinz on 25.10.23.
+//
+
+#ifndef HW03_FORKPIPE_H
+#define HW03_FORKPIPE_H
+
+#endif //HW03_FORKPIPE_H
